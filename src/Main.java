38dc38d85f06e7f886a1db1c8fd6b58e6a64016f
lucasfmerino <lucasfmerino@gmail.com>
2023-11-02 17:09:30 -0300

    feat(models): Controlling acess and writing to application data

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Movie movie = new Movie();
 
-        movie.name = "O Poderoso Chefão";
-        movie.releaseYear = 1970;
-        movie.durationInminutes = 180;
+        movie.setName("O Poderoso Chefão");
+        movie.setReleaseYear(1970);
+        movie.setDurationInMinutes(180);
 
         movie.displaysDatasheet();
 
-        System.out.println(movie.review);
-
-        movie.evaluate(8.2);
-        System.out.println(movie.review);
-
-        movie.evaluate(6.8);
-        System.out.println(movie.review);
-
-        movie.evaluate(9.5);
-        System.out.println(movie.review);
-
-        System.out.println(movie.getReview());
+        if (movie.getNumberOfReviews() == 0) {
+            System.out.println("O filme ainda não possui avaliações");
+        } else {
+            System.out.println(movie.getReview());
+        }
 
     }
 }
--- src/models/Movie.java
+++ src/models/Movie.java
@@ -3,32 +3,80 @@ package models;
  * Movie
  */
 public class Movie {
-    public String name;
-    public boolean includedInThePlan;
-    public double review;
-    public int releaseYear;
-    public int numberOfreviews;
-    public int durationInminutes;
+    private String name;
+    private boolean includedInThePlan;
+    private double review;
+    private int releaseYear;
+    private int numberOfreviews;
+    private int durationInminutes;
 
+// MÉTODOS PARA O ATRIBUTO NOME
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public void displaysDatasheet () {
-        System.out.println("Movie name: " + name);
-        System.out.println("Release year: " + releaseYear);
-        System.out.println("Duration: " + durationInminutes);
+    public String getName() {
+        return this.name;
     }
 
 
+// MÉTODOS PARA O ATRUBUTO ANO DE LANÇAMENTO
+    public void setReleaseYear(int year) {
+        this.releaseYear = year;
+    }
+
+    public int getReleaseYear() {
+        return this.releaseYear;
+    }
+
+
+// MÉTODOS PARA O ATRUBUTO DURAÇÃOO DO FILME
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInminutes = durationInMinutes;
+    }
+
+    public int getDurationInMinutes() {
+        return this.durationInminutes;
+    }
+
+
+// MÉTODOS PARA O ATRIBUTO DISPONIBILIDADE NO PLANO
+    public void changeAvailability() {
+        if (this.includedInThePlan) {
+            this.includedInThePlan = false;
+        } else {
+            this.includedInThePlan = true;
+        }
+    }
+
+    public boolean getAvailability() {
+        return this.includedInThePlan;
+    }
+
+
+// MÉTODOS RELACIONADOS À AVALIAÇÕES
+    public double getReview() {
+        return review;
+    }
+
+    public int getNumberOfReviews() {
+        return numberOfreviews;
+    }
+
     public void evaluate(double newReview) {
-        review += newReview;
+        this.review += newReview;
         numberOfreviews++;
         if (numberOfreviews > 1) {
-            review /= 2;
+            this.review /= 2;
         }
     }
 
 
-    public double getReview() {
-        return review;
+// MÉTODO PARA EXIBIR FICHA TÉCNICA DO FILME
+    public void displaysDatasheet () {
+        System.out.println("Movie name: " + name);
+        System.out.println("Release year: " + releaseYear);
+        System.out.println("Duration: " + durationInminutes);
     }
 
 }
