330631e980704b7ba42c7d053861f2fbb05ae9fc
lucasfmerino <lucasfmerino@gmail.com>
2023-11-04 01:33:36 -0300

    refactor (Main): modified the instantiation order of classes

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,40 @@ import models.Series;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Movie movie = new Movie();
 
+
+// ---------------------- MOVIES INSTANCES ----------------------
+        Movie movie = new Movie();
         movie.setName("O Poderoso Chefão");
         movie.setReleaseYear(1970);
         movie.setDurationInMinutes(180);
 
-        movie.displaysDatasheet();
 
+
+// ---------------------- SERIES INSTANCES ----------------------
+        Series series = new Series();
+        series.setName("Lost");
+        series.setReleaseYear(2000);
+        series.setSeasons(10);
+        series.setEpisodesPerSeason(10);
+        series.setMinutesPerEpisode(50);
+
+
+
+// ---------------------- EPISODES INSTANCES ----------------------
+        Episode episode = new Episode();
+        episode.setNumber(1);
+        episode.setSeries(series);
+        episode.setViews(467);
+
+
+
+// ---------------------- MODELS TESTING METHODS ----------------------
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        movie.displaysDatasheet();
         if (movie.getNumberOfReviews() == 0) {
             System.out.println("O filme ainda não possui avaliações");
         } else {
@@ -24,60 +50,39 @@ public class Main {
         System.out.println();
         System.out.println();
 
-
-
-
-        // CRIANDO UMA SÉRIE
-        Series series = new Series();
-        series.setName("Lost");
-        series.setReleaseYear(2000);
-        series.setSeasons(10);
-        series.setEpisodesPerSeason(10);
-        series.setMinutesPerEpisode(50);
-
         series.displaysDatasheet();
-
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-
-
-        TimeCalculator timeCalculator = new TimeCalculator();
-
-        timeCalculator.includeTitle(movie);
-
-        System.out.println("O tempo total necessário para assistir esses títulos é de %d minutos".formatted(timeCalculator.getTotalTime()));
-
-        timeCalculator.includeTitle(series);
-
-        System.out.println("O tempo total necessário para assistir esses títulos é de %d minutos".formatted(timeCalculator.getTotalTime()));
-
-
-
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
 
         movie.evaluate(8.5);
         movie.evaluate(5.5);
         movie.evaluate(7);
 
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+// ---------------------- TIME CALCULATOR ----------------------
+        TimeCalculator timeCalculator = new TimeCalculator();
+
+        timeCalculator.includeTitle(movie);
+        System.out.println("O tempo total necessário para assistir esses títulos é de %d minutos".formatted(timeCalculator.getTotalTime()));
+
+        timeCalculator.includeTitle(series);
+        System.out.println("O tempo total necessário para assistir esses títulos é de %d minutos".formatted(timeCalculator.getTotalTime()));
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+
+// ---------------------- RECOMMENDATIONS ----------------------
         RecommendationFilter filter = new RecommendationFilter();
+
         filter.filters(movie);
         System.out.println(movie.getRating());
         System.out.println(movie.getReview());
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-        Episode episode = new Episode();
-        episode.setNumber(1);
-        episode.setSeries(series);
-        episode.setViews(467);
-        
         System.out.println(episode.getRating());
         filter.filters(episode);
 
